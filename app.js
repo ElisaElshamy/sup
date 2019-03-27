24e435b07235249fd075f27f02ddcae0c1298261
Elisa Elshamy <elisa9el@gmail.com>
2019-03-27 00:56:12 -0400

    Client socket created when client loads the page

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ app.use(express.static('public'));
 
 //routes
 app.get('/', (req, res) => {
-    res.send('Hello World');
+    res.render('index');
 });
 
 ///Listen on port 3000
 server = app.listen(3000);
+
+//socket.io instantiation
+const io = require("socket.io")(server)
+
+//listen on every connection
+io.on('connection', (socket) => {
+    console.log('New user connected');
+});
